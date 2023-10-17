670a5ae405a58cab37825045dd5e010ebeafa856
Mate0r <94843357+Mate0r@users.noreply.github.com>
2023-10-17 13:27:04 +0200

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,14 @@
 
+window.addEventListener('load', function() {
+
+    this.window.setTimeout(function() {
+        
+        const urlParams = new URLSearchParams(window.location.search);
+        let player = document.querySelector("#player.ytd-watch-flexy");
+        var height = player.offsetHeight;
+        player.innerHTML = '<iframe class="youtube-unblock" src="https://www.youtube.com/embed/' + urlParams.get('v') + '?autoplay=1" allow="autoplay"></iframe>';
+        document.querySelector("iframe.youtube-unblock").style.width = "100%";
+        document.querySelector("iframe.youtube-unblock").style.height = height + "px";
+    }, 500);
+    
+});
